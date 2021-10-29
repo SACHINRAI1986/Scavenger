85c6755c559de63c5e54b0e535a427b7ace69c20
Sachin rai <76139043+SACHINRAI1986@users.noreply.github.com>
2021-10-30 00:44:50 +0530

    Delete Scavenger directory

--- Scavenger/Scavenger Package/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 %~dp0Scavenger.inf
-sc start Scavenger
-pause
--- Scavenger/Scavenger Package/uninstall.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultUninstall 132 %~dp0Scavenger.inf
-pause
